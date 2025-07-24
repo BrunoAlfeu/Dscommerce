2e662d6a29dd1ac08c7bd18b905880fcbe3c09ad
Bruno Alfeu <brunoalfeu213@gmail.com>
2025-07-23 22:02:53 -0300

    feat(seed): add initial seed data for development

--- src/main/java/com/brunoalfeu/dscommerce/entities/OrderItemPK.java
+++ src/main/java/com/brunoalfeu/dscommerce/entities/OrderItemPK.java
@@ -18,24 +18,24 @@ public class OrderItemPK {
     public OrderItemPK() {
     }
 
-    public OrderItemPK(Order order, Product product) {
-        this.order = order;
-        this.product = product;
-    }
 
     public Order getOrder() {
+
         return order;
     }
 
     public void setOrder(Order order) {
+
         this.order = order;
     }
 
     public Product getProduct() {
+
         return product;
     }
 
     public void setProduct(Product product) {
+
         this.product = product;
     }
 }
